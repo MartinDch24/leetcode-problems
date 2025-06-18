d4195740e48de4736c5748094453444f8b993e95
MartinDch24 <martindoychev2007@gmail.com>
2025-06-19 00:21:46 +0300

    LeetCode problem 841

---
+++ 841.keys_and_rooms.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+class Solution(object):
+    def canVisitAllRooms(self, rooms):
+        """
+        :type rooms: List[List[int]]
+        :rtype: bool
+        """
+        q = deque([0])
+        visited = {0}
+
+        while q:
+            node = q.popleft()
+
+            for neighbor in rooms[node]:
+                if neighbor not in visited:
+                    visited.add(neighbor)
+                    q.append(neighbor)
+
+        return len(visited) == len(rooms)
