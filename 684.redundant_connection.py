446acf4668e06b9c9df11f7f2662ff0ea1d82432
MartinDch24 <martindoychev2007@gmail.com>
2025-06-22 21:19:31 +0300

    LeetCode problem 684 - Union Find solution

--- 684.redundant_connection.py
+++ 684.redundant_connection.py
@@ -26,3 +26,21 @@ class Solution(object):
 
             graph[a].append(b)
             graph[b].append(a)
+
+        #Union Find solution:
+        # n = len(edges)
+        #
+        # parent = [i for i in range(n + 1)]
+        # for a, b in edges:
+        #     ra = a
+        #     while parent[ra] != ra:
+        #         parent[ra], ra = parent[parent[ra]], parent[ra]
+        #
+        #     rb = b
+        #     while parent[rb] != rb:
+        #         parent[rb], rb = parent[parent[rb]], parent[rb]
+        #
+        #     if ra == rb:
+        #         return [a, b]
+        #
+        #     parent[ra] = rb
