1a764e16a00b2d76ce2bf536ab8b0be9b29ecfac
MartinDch24 <martindoychev2007@gmail.com>
2025-06-27 01:05:50 +0300

    LeetCode problem 1631 - union find solution

--- 1631.path_with_minimum_effort.py
+++ 1631.path_with_minimum_effort.py
@@ -31,3 +31,37 @@ class Solution(object):
                     if new_effort < min_effort[nr][nc]:
                         min_effort[nr][nc] = new_effort
                         heapq.heappush(heap, (new_effort, nr, nc))
+
+
+        #Union Find solution:
+        # m = len(heights)
+        # n = len(heights[0])
+        # parent = list(range(m * n))
+        #
+        # def find(x):
+        #     while x != parent[x]:
+        #         parent[x] = parent[parent[x]]
+        #         x = parent[x]
+        #     return x
+        #
+        # def union(x, y):
+        #     parent[find(x)] = find(y)
+        #
+        # edges = []
+        # for r in range(m):
+        #     for c in range(n):
+        #         i = r * n + c  # flatten (r, c) to a 1D index for Union Find
+        #         if r + 1 < m:
+        #             edges.append((abs(heights[r][c] - heights[r + 1][c]), i, (r + 1) * n + c))
+        #         if c + 1 < n:
+        #             edges.append((abs(heights[r][c] - heights[r][c + 1]), i, r * n + (c + 1)))
+        #
+        # edges.sort()
+        #
+        # #connect edges starting from the least effort ones, until we've connected (0, 0) and (m-1, n-1)
+        # for effort, u, v in edges:
+        #     union(u, v)
+        #     if find(0) == find(m * n - 1):
+        #         return effort
+        #
+        # return 0
