7f03d2d8f759d00b6424e52b83849da4c9f1af5d
MartinDch24 <martindoychev2007@gmail.com>
2025-06-29 01:51:52 +0300

    Resolved LeetCode problem 112

--- 112.path_sum.py
+++ 112.path_sum.py
@@ -1,5 +1,4 @@
-from collections import deque
-
+#Resolved
 
 # Definition for a binary tree node.
 # class TreeNode(object):
@@ -17,34 +16,36 @@ class Solution(object):
         if not root:
             return False
 
-        q = deque([(root, root.val)])
+        s = [(root, root.val)]  #We use a stack for a DFS solution. In it, we save the node and the sum so far.
 
-        while q:
-            for _ in range(len(q)):
-                cur, cur_sum = q.popleft()
+        while s:
+            node, curr_sum = s.pop()
 
-                if not cur.left and not cur.right and cur_sum == targetSum:
+            if not node.left and not node.right:    # If the node is a leaf, check if the accumulated sum equals targetSum.
+                if curr_sum == targetSum:           # If it does, return True; otherwise, keep searching.
                     return True
+                continue
 
-                if cur.left:
-                    q.append((cur.left, cur_sum + cur.left.val))
-                if cur.right:
-                    q.append((cur.right, cur_sum + cur.right.val))
+            if node.left:   #If the node has a node left of it, we append it to the stack, along with the new sum
+                s.append((node.left, curr_sum + node.left.val))
+            if node.right:  #If the node has a node right of it, we append it to the stack, along with the new sum
+                s.append((node.right, curr_sum + node.right.val))
 
         return False
 
-
-        # s = [(root, root.val)]
+        #BFS solution:
+        # q = deque([(root, root.val)])
         #
-        # while s:
-        #     cur, cur_sum = s.pop()
+        # while q:
+        #     for _ in range(len(q)):
+        #         cur, cur_sum = q.popleft()
         #
-        #     if not cur.left and not cur.right and cur_sum == targetSum:
-        #         return True
+        #         if not cur.left and not cur.right and cur_sum == targetSum:
+        #             return True
         #
-        #     if cur.right:
-        #         s.append((cur.right, cur_sum + cur.right.val))
-        #     if cur.left:
-        #         s.append((cur.left, cur_sum + cur.left.val))
+        #         if cur.left:
+        #             q.append((cur.left, cur_sum + cur.left.val))
+        #         if cur.right:
+        #             q.append((cur.right, cur_sum + cur.right.val))
         #
         # return False
