7e7a76de18933ac7fa302581204d097f198fa482
MartinDch24 <martindoychev2007@gmail.com>
2025-06-28 20:29:24 +0300

    Resolved LeetCode problem 1514

--- 1514.path_with_maximum_probability.py
+++ 1514.path_with_maximum_probability.py
@@ -1,3 +1,4 @@
+#Resolved
 import heapq
 import math
 from collections import defaultdict
@@ -14,29 +15,36 @@ class Solution(object):
         :rtype: float
         """
         graph = defaultdict(list)
-        for i, (a, b) in enumerate(edges):
-            prob = succProb[i]
-            if prob == 0.0:
-                continue
-            log_prob = math.log(prob)
-            graph[a].append((b, log_prob))
-            graph[b].append((a, log_prob))
 
-        heap = [(-0.0, start_node)]
-        best = [-float('inf')] * n
-        best[start_node] = 0.0
+        for i, edge in enumerate(edges):
+            a, b = edge
+            prob = succProb[i]
+
+            if prob == 0.0:  # log(0) is undefined
+                continue
+
+            log_of_prob = math.log(prob)  # We use natural logarithms for better end result precision
+            graph[a].append((b, log_of_prob))
+            graph[b].append((a, log_of_prob))
+
+        heap = [(0.0, start_node)]  # We start with the starting node and a probability of 1, the log of which is 0
+        max_prob = [-float('inf')] * n
+        max_prob[start_node] = 0.0
 
         while heap:
-            curr_log_prob_neg, node = heapq.heappop(heap)
-            curr_log_prob = -curr_log_prob_neg
+            curr_prob_neg, node = heapq.heappop(heap)
+            curr_prob = -curr_prob_neg  # Convert the probability back to normal
 
             if node == end_node:
-                return math.exp(curr_log_prob)
+                return math.exp(curr_prob)  # We return the exponent of the maximum probability
 
-            for neighbor, edge_log_prob in graph[node]:
-                new_log_prob = curr_log_prob + edge_log_prob
-                if new_log_prob > best[neighbor]:
-                    best[neighbor] = new_log_prob
-                    heapq.heappush(heap, (-new_log_prob, neighbor))
+            for neighbor, prob in graph[node]:
+                new_prob = curr_prob + prob  # log(a) + log(b) = log(a*b)
+
+                if new_prob > max_prob[neighbor]:
+                    max_prob[neighbor] = new_prob
+                    heapq.heappush(heap, (-new_prob, neighbor))
+
+                    # We save the probability as a negative so the priority queue can be max priority, instead of min
 
         return 0.0
