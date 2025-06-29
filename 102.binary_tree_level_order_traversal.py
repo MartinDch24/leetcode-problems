b23ed83d20cec5f53b3bdc437044d9759c1d7353
MartinDch24 <martindoychev2007@gmail.com>
2025-06-30 00:50:06 +0300

    Resolved LeetCode problem 102

--- 102.binary_tree_level_order_traversal.py
+++ 102.binary_tree_level_order_traversal.py
@@ -1,4 +1,7 @@
+#Resolved
 from collections import deque
+
+
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
@@ -11,25 +14,25 @@ class Solution(object):
         :type root: Optional[TreeNode]
         :rtype: List[List[int]]
         """
-        if not root:
+        if not root: #If we don't explicitly check for this, node will be None and when we append to the level node.val will throw an error
             return []
 
-        q = deque([root])
-        res = []
+        q = deque([root]) #We initialize a queue to perform BFS, so we can explore level by level
+        result = []
 
         while q:
-            node_count = len(q)
             level = []
 
-            for _ in range(node_count):
+            for _ in range(len(q)): #By iterating through every element currently in the queue on every while iteration, we get each level
                 node = q.popleft()
+
                 level.append(node.val)
 
-                if node.left:
+                if node.left:   #These will be able to be processed only in the next while iteration, because of the for loop having the old length of the queue as a range
                     q.append(node.left)
                 if node.right:
                     q.append(node.right)
 
-            res.append(level)
+            result.append(level)
 
-        return res
+        return result
