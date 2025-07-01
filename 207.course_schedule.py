528d8a8fec2853541a8f9731943c15f99a525e3a
MartinDch24 <martindoychev2007@gmail.com>
2025-07-01 14:41:50 +0300

    Resolved LeetCode problem 207

--- 207.course_schedule.py
+++ 207.course_schedule.py
@@ -1,6 +1,5 @@
-from collections import deque, defaultdict
-
-
+#Resolved
+from collections import defaultdict, deque
 class Solution(object):
     def canFinish(self, numCourses, prerequisites):
         """
@@ -8,27 +7,27 @@ class Solution(object):
         :type prerequisites: List[List[int]]
         :rtype: bool
         """
-        graph = defaultdict(list)
-        in_degree = [0] * numCourses
-        taken_courses = 0
+        graph = defaultdict(list)   # node: [neighbor1, ...]
+        in_degree = [0] * numCourses    # How many courses need to be taken before course i
+        courses_taken = 0
 
-        for course, req in prerequisites:
-            graph[req].append(course)
-            in_degree[course] += 1
+        for a, b in prerequisites:
+            graph[b].append(a)  # Course a is dependent on course b
+            in_degree[a] += 1   # Course a has +1 course to be taken before it
 
-        q = deque(i for i in range(numCourses) if in_degree[i] == 0)
+        q = deque(i for i in range(numCourses) if not in_degree[i]) # We initialize the queue with all independent courses
 
         while q:
-            for _ in range(len(q)):
-                cur = q.popleft()
-                taken_courses += 1
+            course = q.popleft()
+            courses_taken += 1
 
-                for course in graph[cur]:
-                    in_degree[course] -= 1
-                    if in_degree[course] == 0:
-                        q.append(course)
+            for dependent in graph[course]:
+                in_degree[dependent] -= 1   # We take the popped course and deduct it from its neighbor's dependencies
+                if in_degree[dependent] == 0:
+                    q.append(dependent) # If a course has no more dependencies, we append it to the queue
 
-        return taken_courses == numCourses
+        return courses_taken == numCourses  # If there are cycles in the graph, courses_taken will be less than numCourses
+
 
     #Iterative DFS solution with cycle detection:
     # graph = defaultdict(list)
