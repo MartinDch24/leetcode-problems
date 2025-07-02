6be04ba71100e143d880d147e8cf2a744915fcd7
MartinDch24 <martindoychev2007@gmail.com>
2025-07-02 17:58:53 +0300

    Resolve LeetCode problem 752

--- 752.open_the_lock.py
+++ 752.open_the_lock.py
@@ -1,6 +1,4 @@
-from collections import deque
-
-
+#Resolved
 class Solution(object):
     def openLock(self, deadends, target):
         """
@@ -8,34 +6,43 @@ class Solution(object):
         :type target: str
         :rtype: int
         """
-        def get_vars(num):
-            res = []
+        def get_neighbors(code):    # Iterate over every digit, change it by 1 and return the new code
             for i in range(4):
-                digit = int(num[i])
-                for delta in [-1, 1]:
-                    new_digit = (digit + delta) % 10
-                    new_num = num[:i] + str(new_digit) + num[i+1:]
-                    res.append(new_num)
-            return res
+                digit = int(code[i])
+                for move in (-1, 1):
+                    new_digit = (digit + move) % 10
+                    yield code[:i] + str(new_digit) + code[i+1:]
 
-        q = deque(['0000'])
-        visited = set(deadends)
-        turns = 0
+        dead = set(deadends)    # Create a set for the deadends for faster checks
 
-        if '0000' in visited:
+        if '0000' in dead:
             return -1
+        if '0000' == target:
+            return 0
 
-        while q:
-            for _ in range(len(q)):
-                current = q.popleft()
-                if current == target:
-                    return turns
-                for num in get_vars(current):
-                    if num in visited:
-                        continue
+        start = set(['0000'])   # We will try to meet start and end in the middle, using bidirectional BFS
+        end = set([target])
+        visited = set(['0000'])
+        steps = 0
 
-                    visited.add(num)
-                    q.append(num)
-            turns += 1
+        while start and end:
+            if len(start) > len(end):   # We always expand the smaller set
+                start, end = end, start
+
+            next_level = set()  # Build a new set for start, so we go through less numbers in checks
+
+            for code in start:
+                if code in dead:
+                    continue
+
+                for neighbor in get_neighbors(code):
+                    if neighbor in end: # Check if the next possible combination is already in end, i.e. start and end intersect
+                        return steps + 1
+                    if neighbor not in visited and neighbor not in dead:
+                        visited.add(neighbor)
+                        next_level.add(neighbor)
+
+            start = next_level  # Replace start with the new set
+            steps += 1
 
         return -1
