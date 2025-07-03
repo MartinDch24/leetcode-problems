22042ac2f13f59bd8146eb482209ab5c1e6fd4c8
MartinDch24 <martindoychev2007@gmail.com>
2025-07-03 14:16:59 +0300

    Resolved LeetCode problem 3

--- 3.longest_substring_without_repeating_characters.py
+++ 3.longest_substring_without_repeating_characters.py
@@ -1,22 +1,25 @@
+#Resolved
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
         """
         :type s: str
         :rtype: int
         """
-        if not s:
+        if not s:  # Skip initializing max_len, left and the set
             return 0
 
-        unique_chars = set()
+        unique_chars = set()  # Store all unique characters found so far
         max_len = 0
-        left = 0
 
-        for right, char in enumerate(s):
-            while char in unique_chars:
-                unique_chars.remove(s[left])
+        left = 0  # The first index of the substring
+        for right, c in enumerate(s):  # We call the index right, so left makes more sense
+            while c in unique_chars:  # While new character is already in the substring
+                unique_chars.remove(s[
+                                        left])  # Start removing characters from the substring, left to right, until there no longer is a duplicate
                 left += 1
 
-            unique_chars.add(char)
-            max_len = max(max_len, right - left + 1)
+            unique_chars.add(c)  # Finally add the new character, the previous instance of which we removed
+
+            max_len = max(max_len, right - left + 1)  # We add +1, because left and right are indices
 
         return max_len
