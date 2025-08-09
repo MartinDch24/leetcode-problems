aff990b4cc8a62a506431afa447d084a72da4cc6
MartinDch24 <martindoychev2007@gmail.com>
2025-08-10 00:35:04 +0300

    Resolved LeetCode problem 14

--- 14.longest_common_prefix.py
+++ 14.longest_common_prefix.py
@@ -1,14 +1,12 @@
-class Solution(object):
-    def longestCommonPrefix(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: str
-        """
-        shortest_word = min(strs, key=len)
+#Resolved
+from typing import List
 
-        while shortest_word:
-            if all(string.startswith(shortest_word) for string in strs):
-                return shortest_word
 
-            shortest_word = shortest_word[:-1]
-        return ""
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        shortest_word = min(strs, key=len)  # Take the shortest word in the lest and start from it as the longest possible prefix
+
+        while not all(word.startswith(shortest_word) for word in strs): # While there are words that don't have shortest_word as a prefix
+            shortest_word = shortest_word[:-1]  # Remove the last character of shortest word by slicing up to it
+
+        return shortest_word
