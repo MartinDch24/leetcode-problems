fec6a12ef1e978e37c33d74cd34058a9c13a738a
MartinDch24 <martindoychev2007@gmail.com>
2025-08-10 23:59:32 +0300

    LeetCode problem 509

---
+++ 509.fibonacci_number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        if n < 2:
+            return n
+
+        dp = [0] * (n + 1)  # A table to save all values, where dp[i] = F(i)
+        dp[1] = 1
+
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]  # Fill in the table
+
+        return dp[n]
