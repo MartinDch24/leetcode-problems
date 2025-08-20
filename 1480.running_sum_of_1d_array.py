6cd2fbfbeff693f2499320d229601051114cab19
MartinDch24 <martindoychev2007@gmail.com>
2025-08-20 22:48:58 +0300

    Resolved LeetCode problem 1480

--- 1480.running_sum_of_1d_array.py
+++ 1480.running_sum_of_1d_array.py
@@ -1,15 +1,22 @@
-class Solution(object):
-    def runningSum(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        n = len(nums)
-        acc = 0
-        res = [0] * n
+#Resolved
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        for i in range(1, len(nums)):   # Iterate from the 2nd to the last index
+            nums[i] += nums[i-1]    # Calculate the prefix sum of each number by adding the previous element to the current one
+        return nums
 
-        for i in range(n):
-            acc += nums[i]
-            res[i] = acc
-
-        return res
+# class Solution(object):
+#     def runningSum(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: List[int]
+#         """
+#         n = len(nums)
+#         acc = 0
+#         res = [0] * n
+#
+#         for i in range(n):
+#             acc += nums[i]
+#             res[i] = acc
+#
+#         return res
