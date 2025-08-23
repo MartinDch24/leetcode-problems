bf849a954ad12158681719dc85c9ae81cf325632
MartinDch24 <martindoychev2007@gmail.com>
2025-08-23 20:24:20 +0300

    Resolved LeetCode problem 198

--- 198.house_robber.py
+++ 198.house_robber.py
@@ -1,14 +1,16 @@
+#Resolved
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        n = len(nums)
-        if n == 1:
+        # We have n number of houses
+        if len(nums) == 1:  # If there's only one house, just rob it
             return nums[0]
 
-        dp = [0] * n    # Array to store the max money for i+1 houses
-        dp[0] = nums[0] # We have only 1 house, so the most money we can get will be by robbing it
-        dp[1] = max(nums[0], nums[1])   # We have only 2 houses and since they'll be adjacent, we rob the one with more money
+        # prev2 = max money if we stop 2 houses back
+        # prev1 = max money if we stop at the previous house
+        prev2, prev1 = 0, 0
 
-        for i in range(2, n):
-            dp[i] = max(dp[i-1], dp[i-2] + nums[i]) # For house i, we have the choice of skipping it (thus max money being dp[i-1]) or robbing it (which will mean forfeiting the previous house, since it's adjacent and robbing the house i-2 (the one left of i-1), making max money dp[i-2] + whatever money house i has)
+        for num in nums:
+            curr = max(prev2+num, prev1)    # The number of houses so far. We use max() to check whether we'd get more money by robbing the current house and the one 2 before it (not adjacent) or by skipping it and settling for robbing the previous house
+            prev2, prev1 = prev1, curr  # Move the window forward
 
-        return dp[-1]   # After building dp for every i, we return the max amount of money for n houses, where n is the length of the inputet array
+        return prev1 # The max amount of money for n houses
