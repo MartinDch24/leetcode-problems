27dff3e8d50c8bd682216e29fd0b394b70bbcfec
MartinDch24 <martindoychev2007@gmail.com>
2025-09-04 22:28:21 +0300

    LeetCode problem 283

---
+++ 283.move_zeroes.py
@@ -0,0 +1,16 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        pos = 0 # The position the first found non-zero should go
+
+        for i in range(n):
+            if nums[i] != 0:    # When we find a non-zero, we put it at pos and increment pos
+                nums[pos] = nums[i]
+                pos += 1
+
+        while pos < n:  # If pos is less than len(nums), that means that we should put the 0 we overwrote earlier at nums[pos] and increment it, until we have filled the array
+            nums[pos] = 0
+            pos += 1
