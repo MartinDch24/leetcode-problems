1b7b5529043b0f3a75af30bfadbe14536040a97c
MartinDch24 <martindoychev2007@gmail.com>
2025-09-27 15:46:40 +0300

    Resolved LeetCode problem 121 again

--- 121.best_time_to_buy_and_sell_stock.py
+++ 121.best_time_to_buy_and_sell_stock.py
@@ -1,16 +1,11 @@
-#Resolved
-class Solution(object):
-    def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
-        min_price = prices[0]   # Minimum price found so far
+#Resolved - 2
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = prices[0]   # Smallest price so far
         max_profit = 0  # Largest possible profit
 
         for price in prices:
-            min_price = min(min_price, price)   # If the current price is less than min_price, it becomes the new min_price
-
-            max_profit = max(max_profit, price-min_price)   # We check whether selling right now will yield more profit
+            min_price = min(min_price, price)
+            max_profit = max(max_profit, price - min_price) # See if selling on the current day will yield a larger profit
 
         return max_profit
