eddbddc4b504dc742c27c4ee3c066306c928d8a0
MartinDch24 <martindoychev2007@gmail.com>
2025-09-28 23:42:20 +0300

    Resolved LeetCode problem 322

--- 322.coin_change.py
+++ 322.coin_change.py
@@ -1,16 +1,16 @@
+#Resolved
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        min_coins = [float("inf")] * (amount+1)  # min_coins[<amount1>] = <min coins to get that amount> and so on...
-        min_coins[0] = 0    # We start with 0 coins for amount 0
+        min_coins = [float("inf")] * (amount+1) # The minimum nuber of coins needed for every amount up to the given one
+        min_coins[0] = 0    # We set the amount of 0 to 0 coins needed
 
-        for i in range(1, amount + 1):
+        for i in range(amount+1):
             for c in coins:
+                # If the current coin isn't larger in value then the amount we want, we look for the minimum amount of coins needed to get i minus the current coin
+                # We will either have precomputed the value earlier or it will just be float("inf")
+                # After we get the minimum coins needed for amount i-c, we add 1, which is the new coin, needed to get amount i
                 if i - c >= 0:
-                    # If i-c < 0, we can't get it with any coins
+                    min_coins[i] = min(min_coins[i], min_coins[i-c] + 1)
 
-                    # 1 new coin + the smallest number of coins to get the value of (i - the value of the new coin)
-                    # If we can't get the amount of i-c with any coins, then min_coins[i] will just remain float("inf")
-                    min_coins[i] = min(min_coins[i], 1 + min_coins[i-c])
-
-        # If we couldn't find a viable way to build to amount, then min_val remains float("inf") and min_coins[amount] has a value of float("inf")
+        # We either managed to get amount with the given coins or it remained float("inf")
         return -1 if min_coins[amount] == float("inf") else min_coins[amount]
