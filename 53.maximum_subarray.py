98250fc57b9db56efd3f32a526173f563d45e55f
MartinDch24 <martindoychev2007@gmail.com>
2025-10-07 23:54:23 +0300

    LeetCode problem 53 - Kadane's algorithm

--- 53.maximum_subarray.py
+++ 53.maximum_subarray.py
@@ -11,3 +11,17 @@ class Solution:
             dp[i] = max(dp[i-1] + nums[i], nums[i])
 
         return max(dp)  # Return the largest sum of the calculated ones
+
+        #Kadane's algorithm:
+
+        # curr_sum = 0  # The largest sum ending at the current number
+        # max_sum = nums[0]  # The largest sum
+        #
+        # for num in nums:
+        #     # Either the current sum + the new number or just the new number
+        #     curr_sum = max(curr_sum + num, num)
+        #
+        #     # Check if the current sum is larger than the max_sum so far
+        #     max_sum = max(curr_sum, max_sum)
+        #
+        # return max_sum
