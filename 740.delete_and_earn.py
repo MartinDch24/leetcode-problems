7038dd9db6210e55a3a2aa0b1fad7444773f6a09
MartinDch24 <martindoychev2007@gmail.com>
2025-10-11 18:03:20 +0300

    LeetCode problem 740 - O(1) space solution

--- 740.delete_and_earn.py
+++ 740.delete_and_earn.py
@@ -19,3 +19,24 @@ class Solution:
             )
 
         return earnings[max_num]
+
+    # O(1) space solution:
+    #
+    # if not nums:
+    #     return 0
+    #
+    # freq_map = Counter(nums)  # {<num>: <times it appears>, ...}
+    # max_num = max(nums)  # The largest number in the array
+    #
+    # # The max points for i-2 nums and i-1 nums
+    # prev2, prev1 = 0, 0
+    #
+    # for i in range(max_num + 1):
+    #     # Either the max points for i-1 nums or the max points of i-2 nums + the points that deleting i would give
+    #     curr = max(prev1, prev2 + freq_map.get(i, 0) * i)
+    #
+    #     # Move the window forward
+    #     prev2 = prev1
+    #     prev1 = curr
+    #
+    # return prev1  # prev1 is equal to curr at the end, but curr isn't initialized outside the loop
