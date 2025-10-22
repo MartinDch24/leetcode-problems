6528417c25a4ba0d61219d4fff1a452ec245f074
MartinDch24 <martindoychev2007@gmail.com>
2025-10-23 00:52:24 +0300

    Resolved LeetCode problem 70

--- 70.climbing_stairs.py
+++ 70.climbing_stairs.py
@@ -1,13 +1,12 @@
+#Resolved
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n <= 2:  # The base cases 0, 1 and 2
+        if n <= 2:  # Edge cases
             return n
 
-        dp = [0] * (n + 1)  # A table where dp[i] = number of ways to reach step i
-        dp[1], dp[2] = 1, 2  # Put in the values of the base cases
+        dp = [1] * (n+1)    # dp[i] = ways to climb i stairs
 
-        for i in range(3, n + 1):
-            dp[i] = dp[i - 1] + dp[
-                i - 2]  # The number of ways to reach step n is the ways to reach step n-1 and the ways to reach step n-2 summed together
+        for i in range(2, n+1):
+            dp[i] = dp[i-2] + dp[i-1]   # Climb up to step i from either step i-1 or step i-2
 
         return dp[n]
