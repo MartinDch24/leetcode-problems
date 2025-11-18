e661e71e5746d5f484179fb205ba86bbe095310b
MartinDch24 <martindoychev2007@gmail.com>
2025-11-18 22:58:21 +0200

    LeetCode problem 875
    LeetCode problem 875

---
+++ 875.koko_eating_bananas.py
@@ -0,0 +1,22 @@
+from math import ceil
+
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        left = 1    # Minimum value for k
+        right = max(piles)  # Maximum value for k
+
+        while left <= right:
+            mid = (left+right) // 2
+            hours_needed = 0
+
+            # Check the current candidate for k
+            for pile in piles:
+                hours_needed += ceil(pile / mid)
+
+            if hours_needed <= h:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return left
