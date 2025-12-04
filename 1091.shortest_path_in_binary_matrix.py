094196afe3ba45e9531c62ad08ffcd11e83cf18d
MartinDch24 <martindoychev2007@gmail.com>
2025-12-04 22:14:47 +0200

    Resolved LeetCode problem 1091

--- 1091.shortest_path_in_binary_matrix.py
+++ 1091.shortest_path_in_binary_matrix.py
@@ -1,36 +1,30 @@
 from collections import deque
 
 
-class Solution(object):
-    def shortestPathBinaryMatrix(self, grid):
-        """
-        :type grid: List[List[int]]
-        :rtype: int
-        """
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
         if grid[0][0] == 1 or grid[-1][-1] == 1:
             return -1
+        # Directions we can move in
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1),
+                      (-1, -1), (-1, 1), (1, -1), (1, 1)]
 
         n = len(grid)
-        grid[0][0] = 1
-        q = deque([(0, 0)])
-        path = 1
-        directions = [(-1, -1), (-1, 0),
-                      (-1, 1), (0, -1),
-                      (0, 1), (1, -1),
-                      (1, 0), (1, 1)]
+        q = deque([(0, 0, 1)])  # Enqueue the row and column of the start and the current path length
 
         while q:
-            for _ in range(len(q)):
-                row, col = q.popleft()
-                if (row, col) == (n - 1, n - 1):
-                    return path
+            r, c, length = q.popleft()
 
-                for dr, dc in directions:
-                    r, c = row + dr, col + dc
-                    if 0 <= r < n and 0 <= c < n and grid[r][c] == 0:
-                        q.append((r, c))
-                        grid[r][c] = 1
+            # If we've reached the bottom right corner, return the length we've accumulated
+            if r == n-1 and c == n-1:
+                return length
 
-            path += 1
+            for rd, cd in directions:
+                # Check each possible direction we can move in
+                # Validate new coordinates and whether it's visited
+                if 0 <= r+rd < n and 0 <= c+cd < n and grid[r+rd][c+cd] == 0:
+                    grid[r+rd][c+cd] = 1    # Mark the cell as visited
+                    q.append((r+rd, c+cd, length+1))    # Append it to the queue
 
+        # If we've processed all cells we can visit and didn't reach grid[n-1][n-1]
         return -1
