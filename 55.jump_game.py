5ca9770a6270bb7ea77c1664dbf5a480f17274a4
MartinDch24 <martindoychev2007@gmail.com>
2025-12-08 11:03:11 +0200

    LeetCode problem 55

---
+++ 55.jump_game.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        n = len(nums)
+        max_reach = 0  # The maximum distance we can cover in the array
+
+        for i, jump in enumerate(nums):
+            if i > max_reach:  # If i is out of reach, we can't reach the last index
+                break
+            # Potentially increase max_reach with the current index and it's max jump length
+            max_reach = max(max_reach, i + jump)
+
+        return max_reach >= n - 1  # See if we can reach the end of the array
