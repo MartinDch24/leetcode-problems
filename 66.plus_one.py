ef00fbafb6e403d2987e7cfb21c2e74a575458d8
MartinDch24 <martindoychev2007@gmail.com>
2026-01-03 16:27:07 +0200

    LeetCode problem 66

---
+++ 66.plus_one.py
@@ -0,0 +1,17 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        i = len(digits) - 1 # Start at the last digit
+
+        # Make all 9s into 0s and carry 1 over
+        while digits[i] == 9 and i >= 0:
+            digits[i] = 0
+            i -= 1
+
+        # If i == -1, then the first digit was a 9 and we need to add an extra leading 1
+        if i < 0:
+            return [1] + digits
+
+        # Increment the digit after all the 9s
+        digits[i] += 1
+
+        return digits
