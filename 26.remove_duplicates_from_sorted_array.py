76be800b450166ae7b2dd2607df3b477781540b9
MartinDch24 <martindoychev2007@gmail.com>
2026-01-11 14:58:15 +0200

    Resolved and commented LeetCode problem 26

--- 26.remove_duplicates_from_sorted_array.py
+++ 26.remove_duplicates_from_sorted_array.py
@@ -1,17 +1,14 @@
-class Solution(object):
-    def removeDuplicates(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        if not nums:
+#Resolved
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if not nums:    # No duplicates if there aren't any numbers
             return 0
 
-        i = 0
-
+        i = 0   # The indices of the unique numbers
         for j in range(1, len(nums)):
-            if nums[j] != nums[i]:
-                i+=1
+            # Each time we find a unique number, we place it next to the previous unique one
+            if nums[i] != nums[j]:
+                i += 1
                 nums[i] = nums[j]
 
-        return i+1
+        return i + 1    # Add 1, because i is an index and we want a count
