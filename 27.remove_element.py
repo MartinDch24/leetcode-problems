875ded7f679fec00f88e36f5d26040afe3f70e9a
MartinDch24 <martindoychev2007@gmail.com>
2026-01-15 21:27:46 +0200

    Resolved LeetCode problem 27

--- 27.remove_element.py
+++ 27.remove_element.py
@@ -1,15 +1,14 @@
-class Solution(object):
-    def removeElement(self, nums, val):
-        """
-        :type nums: List[int]
-        :type val: int
-        :rtype: int
-        """
-        k = 0
+#Resolved
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        n = len(nums)
+        i = 0  # The index where the next non-val element should be placed
 
-        for i in range(len(nums)):
-            if nums[i] != val:
-                nums[k] = nums[i]
-                k += 1
+        for j in range(n):
+            # If nums[j] is not val, move it to the next valid position
+            if nums[j] != val:
+                # Change the next number in-line
+                nums[i] = nums[j]
+                i += 1
 
-        return k
+        return i  # Number of elements not equal to val
