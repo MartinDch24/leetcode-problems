f700a68833dd20b40ba288a2079a6fc4c1bb8677
MartinDch24 <martindoychev2007@gmail.com>
2026-01-19 23:49:24 +0200

    Resolved LeetCode problem 169

--- 169.majority_element.py
+++ 169.majority_element.py
@@ -1,19 +1,29 @@
-class Solution(object):
-    def majorityElement(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        candidate = None  # Potential candidate to win the "election"
-        votes = 0  # The current amount of "votes" it has
+#Resolved
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        #O(n) time, O(1) space solution:
+        '''
+        If an element appears more than all other elements combined, it cannot be canceled out completely.
+        Matching elements increase the count, mismatches decrease it.
+        The true majority can never be fully canceled out.
+        '''
+        candidate = None
+        count = 0
 
         for num in nums:
-            if votes == 0:  # If the votes are 0, the new candidate becomes the current number
+            # Switch candidate
+            if count == 0:
                 candidate = num
 
-            if candidate == num:  # If the current number is the candidate, increase the votes
-                votes += 1
-            else:  # Otherwise decrease the votes
-                votes -= 1
+            # Reinforce element
+            if candidate == num:
+                count += 1
+            # Decrease majority of the current canidate
+            else:
+                count -= 1
 
         return candidate
+
+        #Sorting solution:
+        # nums.sort()
+        # return nums[len(nums)//2]
