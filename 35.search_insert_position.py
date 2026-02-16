bb242bcddd2b3a2bcd47f4cd68fc6d3b82b99162
MartinDch24 <martindoychev2007@gmail.com>
2026-02-17 00:41:22 +0200

    Resolved LeetCode problem 35

--- 35.search_insert_position.py
+++ 35.search_insert_position.py
@@ -1,21 +1,17 @@
-class Solution(object):
-    def searchInsert(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
-        left = 0
-        right = len(nums) - 1
+#Resolved
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left = 0    # Leftmost index
+        right = len(nums)-1     # Rightmost index
 
         while left <= right:
-            mid = (left+right) // 2
+            mid = (left + right) // 2
 
-            if nums[mid] == target:
-                return mid
-            elif nums[mid] < target:
-                left = mid + 1
+            # Reduce right boundary if nums[mid] overshoots the target
+            # Otherwise increase the left boundary, so nums[mid] can reach target
+            if target <= nums[mid]:
+                right = mid-1
             else:
-                right = mid - 1
+                left = mid+1
 
-        return left
+        return left # Last valid position for target to be insterted
