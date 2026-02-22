faf7124efb0670cad8f66771791e90976dd18b10
MartinDch24 <martindoychev2007@gmail.com>
2026-02-22 02:04:46 +0200

    LeetCode problem 739

---
+++ 739.daily_temperatures.py
@@ -0,0 +1,15 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        answer = [0] * n
+        stack = []  # Store non-resolved indices (no warmer temperature yet)
+
+        for i in range(n):
+            # Iterate through all of the temperature indices for which you've currently found a warmer day
+            while stack and temperatures[stack[-1]] < temperatures[i]:
+                idx = stack.pop()
+                answer[idx] = i-idx
+
+            stack.append(i)
+
+        return answer
