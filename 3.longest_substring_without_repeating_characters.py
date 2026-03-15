cf9edb3074f9d3c7dce2e40f2285a12b8523de7c
MartinDch24 <martindoychev2007@gmail.com>
2026-03-15 20:42:59 +0200

    LeetCode problem 3 Hashmap solution

--- 3.longest_substring_without_repeating_characters.py
+++ 3.longest_substring_without_repeating_characters.py
@@ -19,3 +19,18 @@ class Solution:
             max_len = max(max_len, right - left + 1)
 
         return max_len
+
+        #Hashmap solution:
+
+        # last_index = defaultdict(lambda: -1)  # last_index[char] = <last index it was at>
+        # left = 0  # Left end of the subarray
+        # max_len = 0
+        #
+        # for right, char in enumerate(s):
+        #     # If last_index[char]> left, then we have a duplicate we are removing with + 1
+        #     left = max(left, last_index[char] + 1)
+        #     last_index[char] = right
+        #
+        #     max_len = max(max_len, right - left + 1)
+        #
+        # return max_len
