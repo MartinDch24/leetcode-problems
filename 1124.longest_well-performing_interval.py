617cfc04897f310356f20b2d8a5b0c3612b8076f
MartinDch24 <martindoychev2007@gmail.com>
2026-04-15 00:28:27 +0300

    LeetCode problem 1124

---
+++ 1124.longest_well-performing_interval.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestWPI(self, hours: List[int]) -> int:
+        # The sum of tiring and non-tiring days, where they are counted as 1 and -1, respectively
+        balance = 0
+        res = 0
+        first = {}   # Save the first occurence of a given balance
+
+        for i, time in enumerate(hours):
+            balance += 1 if time > 8 else -1
+
+            if balance > 0:
+                res = i + 1
+
+            if balance not in first:
+                first[balance] = i
+
+            # We are looking for an old balance that was < balance
+            if balance - 1 in first:
+                res = max(res, i - first[balance - 1])
+
+        return res
