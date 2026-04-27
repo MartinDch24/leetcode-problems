ad7f81bbfcd3ae93a62f1bc6687c7caea25217ed
MartinDch24 <martindoychev2007@gmail.com>
2026-04-28 01:21:12 +0300

    Resolved LeetCode problem 75

--- 75.sort_colors.py
+++ 75.sort_colors.py
@@ -1,18 +1,21 @@
-class Solution(object):
-    def sortColors(self, nums):
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
         """
-        :type nums: List[int]
-        :rtype: None Do not return anything, modify nums in-place instead.
+        Do not return anything, modify nums in-place instead.
         """
-        low, mid, high = 0, 0, len(nums) - 1
+        # We use left to track the color after the last 0
+        # and right to track the color before the first 2
+        # This way we can move all 0s to the left and 2s to right, so only the 1s are left in the middle
+        left, right = 0, len(nums) - 1
+        i = 0
 
-        while mid <= high:
-            if nums[mid] == 0:
-                nums[low], nums[mid] = nums[mid], nums[low]
-                low += 1
-                mid += 1
-            elif nums[mid] == 1:
-                mid += 1
-            else:
-                nums[mid], nums[high] = nums[high], nums[mid]
-                high -= 1
+        while i <= right:
+            if nums[i] == 0:
+                nums[left], nums[i] = nums[i], nums[left]
+                left += 1
+            elif nums[i] == 2:
+                nums[right], nums[i] = nums[i], nums[right]
+                right -= 1
+                continue  # We don't increment after swapping a 2, because we don't know what the current value of nums[i] now is
+
+            i += 1
