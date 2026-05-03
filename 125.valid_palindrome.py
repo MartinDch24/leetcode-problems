c37123fe25af16432e7b1da39bd2b1ea3a45684d
MartinDch24 <martindoychev2007@gmail.com>
2026-05-03 03:08:24 +0300

    LeetCode problem 125 - two pointer solution

--- 125.valid_palindrome.py
+++ 125.valid_palindrome.py
@@ -4,3 +4,20 @@ class Solution:
         res = "".join(char for char in s if char.isalnum()) # Take all characters that are alphanumeric and join them without a space
 
         return res == res[::-1] # Compare the string to it's reversed state
+
+    # Two-pointer solution:
+    # left, right = 0, len(s) - 1
+    # while left < right:
+    #     # Skip irrelevant characters
+    #     while left < right and not s[left].isalnum():
+    #         left += 1
+    #     while left < right and not s[right].isalnum():
+    #         right -= 1
+    #
+    #     if s[left].lower() != s[right].lower():
+    #         return False
+    #
+    #     left += 1
+    #     right -= 1
+    #
+    # return True
