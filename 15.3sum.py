b4bcf2de6bf46f34750d132d32ab40460d1a2ace
MartinDch24 <martindoychev2007@gmail.com>
2026-05-05 00:14:14 +0300

    Resolved LeetCode problem 15 again

--- 15.3sum.py
+++ 15.3sum.py
@@ -1,32 +1,39 @@
-#Resolved
+#Resolved - 2
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         nums.sort()
-        res = []
         n = len(nums)
+        res = []
 
-        for i in range(n - 2):
-            # Remove duplicates
+        for i in range(n - 3):
+            # Skip duplicates
             if i > 0 and nums[i - 1] == nums[i]:
                 continue
 
-            j, k = i + 1, n - 1
-            while j < k:
-                # Take -nums[i] as a target sum for nums[j] + nums[k]
-                if nums[j] + nums[k] == -nums[i]:
-                    res.append([nums[i], nums[j], nums[k]])
+            for j in range(i + 1, n - 2):
+                # Skip dublicates
+                if j > i + 1 and nums[j - 1] == nums[j]:
+                    continue
 
-                    # Remove duplicates
-                    while j < k and nums[j] == nums[j + 1]:
-                        j += 1
-                    while j < k and nums[k - 1] == nums[k]:
-                        k -= 1
+                # Search on the sum of nums[k] and nums[l]
+                k, l = j + 1, n - 1
+                needed_sum = target - nums[i] - nums[j]
 
-                    j += 1
-                    k -= 1
-                elif nums[j] + nums[k] < -nums[i]:
-                    j += 1  # Increase sum
-                else:
-                    k -= 1  # Decrease sum
+                while k < l:
+                    if nums[k] + nums[l] < needed_sum:
+                        k += 1  # Increase sum
+                    elif nums[k] + nums[l] > needed_sum:
+                        l -= 1  # Decrease sum
+                    else:
+                        res.append([nums[i], nums[j], nums[k], nums[l]])
+
+                        # Skip duplicates
+                        while k < l and nums[k] == nums[k + 1]:
+                            k += 1
+                        while k < l and nums[l - 1] == nums[l]:
+                            l -= 1
+
+                        k += 1
+                        l -= 1
 
         return res
