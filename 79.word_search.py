b4447477de12fd21064c110a3b5607fb19053818
MartinDch24 <martindoychev2007@gmail.com>
2026-05-28 21:58:26 +0300

    LeetCode problem 79

---
+++ 79.word_search.py
@@ -0,0 +1,37 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        m = len(board)
+        n = len(board[0])
+
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+        def dfs(r, c, i):
+            if i == len(word):
+                return True
+
+            # Mark the cell as visited
+            temp = board[r][c]
+            board[r][c] = "#"
+
+            for d1, d2 in directions:
+                new_r = r + d1
+                new_c = c + d2
+
+                if 0 <= new_r < m and 0 <= new_c < n:
+                    if board[new_r][new_c] == word[i]:
+                        if dfs(new_r, new_c, i + 1):
+                            board[r][c] = temp
+                            return True
+
+                            # Couldn't contiue the word from this cell, so we move backwards
+            board[r][c] = temp
+            return False
+
+        for r in range(m):
+            for c in range(n):
+                # Try to form the word from all possible start letters
+                if board[r][c] == word[0]:
+                    if dfs(r, c, 1):
+                        return True
+
+        return False
