39ecdb0d9e676d4ac99c4a5800df02f2f0093bf7
MartinDch24 <martindoychev2007@gmail.com>
2026-06-03 16:10:57 +0300

    Resolved LeetCode problem 994 again

--- 994.rotting_oranges.py
+++ 994.rotting_oranges.py
@@ -1,41 +1,37 @@
-#Resolved
+#Resolved - 2
 from collections import deque
 
 
-class Solution(object):
-    def orangesRotting(self, grid):
-        """
-        :type grid: List[List[int]]
-        :rtype: int
-        """
-        q = deque()  # We use a queue so we can do BFS
-        fresh = 0
-        minutes = 0
-
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
         m = len(grid)
         n = len(grid[0])
-        directions = ((1, 0), (-1, 0), (0, 1), (0, -1))  # Directions in which we can move
+        time = 0
+        fresh = 0
 
-        for i in range(m):  # We iterate through every cell
+        directions = ((0, -1), (0, 1), (-1, 0), (1, 0))
+        queue = deque()
+        for i in range(m):
             for j in range(n):
-                if grid[i][j] == 1:  # If we find a 1, we add to the fresh oranges
+                if grid[i][j] == 1:
                     fresh += 1
-                elif grid[i][j] == 2:  # If we find a 2, we add it to the queue
-                    q.append((i, j))
+                elif grid[i][j] == 2:
+                    queue.append((i, j))
 
-        while q and fresh:
-            minutes += 1  # We add a minute for each iteration of the while loop
-            for _ in range(len(q)):  # We go through every rotten orange for the given minute
-                r, c = q.popleft()
+        while queue and fresh:
+            time += 1
+            # For each minute explore the area only adjacent to current rotten oranges
+            for _ in range(len(queue)):
+                r, c = queue.popleft()
 
-                for dr, dc in directions:  # Check each direction of the rotten orange
-                    nr = r + dr
-                    nc = c + dc
+                for d1, d2 in directions:
+                    new_r = r + d1
+                    new_c = c + d2
 
-                    if 0 <= nr < m and 0 <= nc < n:  # Check if the indexes are in range
-                        if grid[nr][nc] == 1:  # If we find a fresh orange, we make it rotten and deduct from fresh
+                    if 0 <= new_r < m and 0 <= new_c < n:
+                        if grid[new_r][new_c] == 1:
+                            grid[new_r][new_c] = 2
+                            queue.append((new_r, new_c))
                             fresh -= 1
-                            grid[nr][nc] = 2
-                            q.append((nr, nc))
 
-        return minutes if fresh == 0 else -1  # If we have gone through every rotten orange in the queue without getting rid of all fresh ones, we return -1
+        return -1 if fresh else time
