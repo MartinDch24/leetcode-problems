0c3bd638e358c197f9330dd41f6c15f8e457c718
MartinDch24 <martindoychev2007@gmail.com>
2026-06-11 23:17:54 +0300

    Resovled LeetCode problem 210

--- 210.course_schedule_ii.py
+++ 210.course_schedule_ii.py
@@ -1,30 +1,24 @@
-from collections import deque, defaultdict
+from collections import defaultdict
 
 
-class Solution(object):
-    def findOrder(self, numCourses, prerequisites):
-        """
-        :type numCourses: int
-        :type prerequisites: List[List[int]]
-        :rtype: List[int]
-        """
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        res = []
         graph = defaultdict(list)
-        in_degree = [0] * numCourses
+        indegree = [0] * numCourses
 
         for a, b in prerequisites:
             graph[b].append(a)
-            in_degree[a] += 1
+            indegree[a] += 1
 
-        res = []
-        q = deque(i for i in range(numCourses) if in_degree[i] == 0)
+        stack = [c for c in range(numCourses) if indegree[c] == 0]
+        while stack:
+            curr = stack.pop()
+            res.append(curr)
 
-        while q:
-            course = q.popleft()
-            res.append(course)
-
-            for dependent in graph[course]:
-                in_degree[dependent] -= 1
-                if in_degree[dependent] == 0:
-                    q.append(dependent)
+            for course in graph[curr]:
+                indegree[course] -= 1
+                if indegree[course] == 0:
+                    stack.append(course)
 
         return res if len(res) == numCourses else []
