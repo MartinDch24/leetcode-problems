20499d7e2a6117fd7a49da17edb7a7ce22b35f4c
Ayali Group <martindoychev2007@gmail.com>
2026-06-24 11:37:57 +0300

    LeetCode problem 787

---
+++ 787.cheapest_flights_within_k_stops.py
@@ -0,0 +1,30 @@
+from collections import defaultdict
+import heapq
+
+
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        graph = defaultdict(list)
+        for a, b, w in flights:
+            graph[a].append([b, w])
+        # [<cumulative weight>, <stops so far>, <curr node>]
+        heap = [[0, 0, src]]
+        # dist[(node, stops)] = <shortest path to node with given stops>
+        dist = {(src, 0): 0}
+        while heap:
+            curr_w, stops, node = heapq.heappop(heap)
+
+            if curr_w > dist.get((node, stops), float('inf')) or stops > k+1:
+                continue
+            
+            # First time we reach dst, is with the shortes distance
+            if node == dst:
+                return curr_w
+
+            for neighbor, w in graph[node]:
+                new_w = curr_w + w
+                if new_w < dist.get((neighbor, stops+1), float('inf')):
+                    dist[(neighbor, stops+1)] = new_w
+                    heapq.heappush(heap, [new_w, stops+1, neighbor])
+        
+        return -1
