fe30400dab444d87f02cebd3ef4b7e738ec10bf2
Ayali Group <martindoychev2007@gmail.com>
2026-06-26 11:21:34 +0300

    Resolved LeetCode problem 778

--- 778.swim_in_rising_water.py
+++ 778.swim_in_rising_water.py
@@ -1,32 +1,34 @@
+#Resolved
 import heapq
 
 
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
-        '''
-        Treat time as edge weights. Every time we want to move, the total weight becomes max(current_time, time_of_new_cell)
-        '''
-        directions = ((-1, 0), (1, 0), (0, -1), (0, 1))
+        # The cost of a path is the maximum elevation encountered so far.
+        
         m = len(grid)
         n = len(grid[0])
+        directions = ((0, -1), (0, 1), (-1, 0), (1, 0))
 
         heap = [(grid[0][0], 0, 0)]
-        dist = {(0, 0): grid[0][0]}
+        visited = set()
 
         while heap:
-            time, r, c = heapq.heappop(heap)
+            t, r, c = heapq.heappop(heap)
 
-            if r == m-1 and c == n-1:
-                return time
-
-            if time > dist.get((r, c), float('inf')):
+            # Add to visited only after popping, so the best path can be found
+            if (r, c) in visited:
                 continue
+            visited.add((r, c))
+
+            # Min-heap guarantess the first time we arrive at the bottom-right cell, it will be through the most efficient path
+            if r == m-1 and c == n-1:
+                return t
 
             for d1, d2 in directions:
                 new_r = r+d1
                 new_c = c+d2
                 if 0 <= new_r < m and 0 <= new_c < n:
-                    new_time = max(grid[new_r][new_c], time)
-                    if new_time  < dist.get((new_r, new_c), float('inf')):
-                            dist[(new_r, new_c)] = new_time
-                            heapq.heappush(heap, (new_time, new_r, new_c))
+                    new_t = max(t, grid[new_r][new_c])
+                    if (new_r, new_c) not in visited:
+                        heapq.heappush(heap, (new_t, new_r, new_c))
