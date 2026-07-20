33c064d7ab266cb8255c189d15f305ea4aca3836
Ayali Group <martindoychev2007@gmail.com>
2026-07-20 10:21:00 +0300

    LeetCode problem 72

---
+++ 72.edit_distance.py
@@ -0,0 +1,26 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        m = len(word1)
+        n = len(word2)
+        # dp[i][j] = <min num of operations to make word1[:i] match word2[:j]>
+        dp = [[0] * (n+1) for _ in range(m+1)]
+
+        # Delete all characters of word1 to match an empty string for word2
+        for i in range(m + 1):
+            dp[i][0] = i
+        # Insert all characters from word2 if word1 is an empty string
+        for j in range(n + 1):
+            dp[0][j] = j
+
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if word1[i-1] == word2[j-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                else:
+                    dp[i][j] = min(
+                        dp[i][j-1], # Insert
+                        dp[i-1][j], # Delete
+                        dp[i-1][j-1] # Replace
+                    ) + 1
+        
+        return dp[m][n]
