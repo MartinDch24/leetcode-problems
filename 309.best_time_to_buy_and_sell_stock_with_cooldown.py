d45e74d2bc824d21b9d61f3f4860bda149d992a0
Ayali Group <martindoychev2007@gmail.com>
2026-08-07 12:25:28 +0300

    LeetCode problem 309 DP solution

--- 309.best_time_to_buy_and_sell_stock_with_cooldown.py
+++ 309.best_time_to_buy_and_sell_stock_with_cooldown.py
@@ -19,3 +19,24 @@ class Solution:
 
         # The max profit is either from the rests or selling
         return max(sold, rest)
+
+        #DP Solution:
+
+        # n = len(prices)
+        # # 0: able to buy, 1: holding stock, 2: cooldown
+        # dp = [[0] * 3 for _ in range(n)]
+
+        # dp[0][0] = 0              # haven't bought anything
+        # dp[0][1] = -prices[0]     # bought stock
+        # dp[0][2] = float("-inf")  # cannot be on cooldown on the first day
+
+        # for i in range(1, n):
+        #     # You can be able to buy if you either didn't buy yesterday or you were on cooldown yesterday
+        #     dp[i][0] = max(dp[i-1][0], dp[i-1][2])
+        #     # You are holding a stock if you were already holding it yesterday or you bought it yesterday
+        #     dp[i][1] = max(dp[i-1][1], dp[i-1][0] - prices[i])
+        #     # To be on cooldown, you need to have sold your stock
+        #     dp[i][2] = dp[i-1][1] + prices[i]
+
+        # # You either end being able to buy or on cooldown, because holding is unrealized value
+        # return max(dp[n-1][0], dp[n-1][2])
