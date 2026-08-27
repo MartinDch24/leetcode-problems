e2597e04600f570fb2ecf097a2ebf6bb62d6bf00
MartinDch24 <144941576+MartinDch24@users.noreply.github.com>
2026-08-27 09:59:55 +0300

    Resolved LeetCode problem 76 again

--- 76.minimum_window_substring.py
+++ 76.minimum_window_substring.py
@@ -1,40 +1,35 @@
-#Resolved
+#Resolved - 2
 from collections import Counter
 
 
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        freq_t = Counter(t)  # {<char of t>: <how many times it appears in t>, ...}
-        freq_window = Counter()  # {<char in current window>: <its count>}
-
-        res = [-1, -1]  # [left, right] of the substring
-        res_len = float('inf')  # the length of the substring
-
-        # Each time we have enough units of a unique char, we add to char
-        # We want to have enough of each unique char to satisfy need
-        have, need = 0, len(freq_t)
-        left = 0
-
-        for right, char in enumerate(s):
-            freq_window[char] += 1
-
-            if char in freq_t and freq_window[char] == freq_t[char]:
-                have += 1
-
-            # Shrink the window, but keep it valid
-            while have == need:
-                # Check if we've found a shorter valid substring
-                if right - left + 1 < res_len:
-                    res = [left, right]
-                    res_len = right - left + 1
-
-                curr = s[left]
-                freq_window[curr] -= 1
-
-                if curr in freq_t and freq_window[curr] < freq_t[curr]:
-                    have -= 1
-                left += 1
-
-        if res_len == float('inf'):
+        m = len(s)
+        n = len(t)
+        if n > m:
             return ""
-        return s[res[0]: res[1] + 1]
+
+        to_find = Counter(t)
+        have = Counter()
+        unique_chars_found = 0
+
+        min_window = float('inf')
+        min_left = 0  # the left pointer of the smallest window we find
+        left = 0
+        for i in range(m):
+            have[s[i]] += 1
+
+            if have[s[i]] == to_find[s[i]]:
+                unique_chars_found += 1
+
+            if unique_chars_found == len(to_find.keys()):
+                # Shrink as much as possible from the left, while preserving all characters of t
+                while have[s[left]] > to_find[s[left]]:
+                    have[s[left]] -= 1
+                    left += 1
+
+                if min_window > i - left + 1:
+                    min_window = i - left + 1
+                    min_left = left
+
+        return s[min_left: min_left + min_window] if min_window != float('inf') else ""
