f980d3f5b09ad3d2617ce9927966cdb68ee3d982
MartinDch24 <martindoychev2007@gmail.com>
2026-08-31 14:20:04 +0300

    Resolved LeetCode problem 994 again

--- 994.rotting_oranges.py
+++ 994.rotting_oranges.py
@@ -1,4 +1,4 @@
-#Resolved - 2
+#Resolved - 3
 from collections import deque
 
 
@@ -6,32 +6,30 @@ class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         m = len(grid)
         n = len(grid[0])
-        time = 0
-        fresh = 0
 
-        directions = ((0, -1), (0, 1), (-1, 0), (1, 0))
-        queue = deque()
+        directions = ((-1, 0), (1, 0), (0, -1), (0, 1))
+        queue = deque([])
+        minutes = 0
+        oranges_left = 0
+
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == 1:
-                    fresh += 1
-                elif grid[i][j] == 2:
+                if grid[i][j] == 2:
                     queue.append((i, j))
+                elif grid[i][j] == 1:
+                    oranges_left += 1
 
-        while queue and fresh:
-            time += 1
-            # For each minute explore the area only adjacent to current rotten oranges
+        # Do BFS, expanding from each rotten orange layer by layer, with every layer being 1 minute
+        while queue and oranges_left:
+            minutes += 1
             for _ in range(len(queue)):
                 r, c = queue.popleft()
 
                 for d1, d2 in directions:
-                    new_r = r + d1
-                    new_c = c + d2
+                    new_r, new_c = r + d1, c + d2
+                    if 0 <= new_r < m and 0 <= new_c < n and grid[new_r][new_c] == 1:
+                        grid[new_r][new_c] = 2
+                        oranges_left -= 1
+                        queue.append((new_r, new_c))
 
-                    if 0 <= new_r < m and 0 <= new_c < n:
-                        if grid[new_r][new_c] == 1:
-                            grid[new_r][new_c] = 2
-                            queue.append((new_r, new_c))
-                            fresh -= 1
-
-        return -1 if fresh else time
+        return minutes if not oranges_left else -1
