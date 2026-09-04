bdca4c20713df1d335d1cf907ca35878b4b2acbd
MartinDch24 <martindoychev2007@gmail.com>
2026-09-04 16:31:15 +0300

    Resolved LeetCode problem 684 with Union-Find again

--- 684.redundant_connection.py
+++ 684.redundant_connection.py
@@ -1,4 +1,4 @@
-# Resolved
+# Resolved - 2
 from collections import deque, defaultdict
 
 
@@ -28,30 +28,27 @@ class Solution(object):
             graph[a].append(b)
             graph[b].append(a)
 
-        #Union Find solution:
-        # class Solution:
-        #     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        #         parent = [x for x in range(len(edges)+1)]
-
-        #         def find(x):
-        #             while parent[x] != x:
-        #                 # Compress the path
-        #                 parent[x] = parent[parent[x]]
-        #                 # Find the root
-        #                 x = parent[x]
-        #             return x
-
-        #         def union(a, b):
-        #             # Get the roots of the 2 nodes
-        #             root_a = find(a)
-        #             root_b = find(b)
-
-        #             # Connect the nodes if they have different roots
-        #             if root_a != root_b:
-        #                 parent[root_b] = root_a
-
-        #         for a, b in edges:
-        #             # Check for the redundant edge
-        #             if find(a) == find(b):
-        #                 return [a, b]
-        #             union(a, b)
+        # Union Find solution:
+            # class Solution:
+            #     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+            #         parent = [x for x in range(len(edges) + 1)]
+            #
+            #         def find(x):
+            #             while parent[x] != x:
+            #                 # Compress path
+            #                 parent[x] = parent[parent[x]]
+            #                 # Continue looking for the root
+            #                 x = parent[parent[x]]
+            #             return x
+            #
+            #         def union(a, b):
+            #             if find(a) == find(b):
+            #                 return True
+            #
+            #             root_a = find(a)
+            #             root_b = find(b)
+            #             parent[root_a] = root_b
+            #
+            #         for a, b in edges:
+            #             if union(a, b):
+            #                 return [a, b]
