d28742eef72d5d367d93d25ae901068247a62532
MartinDch24 <martindoychev2007@gmail.com>
2026-09-07 17:54:58 +0300

    Resolved LeetCode problem 547 again

--- 547.number_of_provinces.py
+++ 547.number_of_provinces.py
@@ -1,24 +1,31 @@
-#Resolved
+#Resolved - 2
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         n = len(isConnected)
-        parent = [i for i in range(n)]
-        # Rank trees by height
-        rank = [0 for _ in range(n)]
-        provinces = set()
+        res = n  # Initially every city is a province
+        parent = [i for i in range(n)]  # The root of every node (city)
+        rank = [1] * n  # Track approximate node tree size, to connected smaller ones to the larger ones
 
         def find(x):
             while x != parent[x]:
+                # Path compression
                 parent[x] = parent[parent[x]]
+                # Search deeper for the root node
                 x = parent[x]
             return x
-        
+
         def union(a, b):
+            nonlocal res
+
             root_a = find(a)
             root_b = find(b)
-                
+
             if root_a != root_b:
-                # Merge the smaller tree into the larger one
+                # Connecting the 2 provinces of which a and b are a part of, combines them into 1 province
+                # thus reducing the number of provinces by 1
+                res -= 1
+
+                # Merge the smaller node tree into the larger one
                 if rank[root_a] > rank[root_b]:
                     parent[root_b] = root_a
                 elif rank[root_a] < rank[root_b]:
@@ -28,9 +35,9 @@ class Solution:
                     rank[root_a] += 1
 
         for i in range(n):
-            for j in range(n):
+            # The matrix is symmetric and there's no point to process both [i][j] and [j][i]
+            for j in range(i + 1, n):
                 if isConnected[i][j]:
                     union(i, j)
 
-        # The provinces are a set of each node's root, so we return its length
-        return len({find(i) for i in range(n)})
+        return res
